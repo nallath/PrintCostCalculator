d18abde4d3ee4f53ceada54efae7f9eb1065463c
Jaime van Kessel <nallath@gmail.com>
2016-06-07 18:13:48 +0200

    Bumped up version number

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ def getMetaData():
         {
             "name": "Print Cost Calculator",
             "author": "Jaime van Kessel",
-            "version": "15.06",
+            "version": "2.1",
             "api": 2,
             "description": i18n_catalog.i18nc("Description of plugin","Extension to quickly calculate print costs")
         }
